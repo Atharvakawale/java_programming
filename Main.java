8e9884472901adac5839f0e96ffe7696b2419d93
Atharvakawale <96365835+Atharvakawale@users.noreply.github.com>
2022-03-08 15:19:18 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Enter first number:");
+    int a=scan.nextInt();
+    System.out.println("Enter second number:");
+    int b=scan.nextInt();
+    int c=a+b;
+    System.out.println("addition of number is=" +c);
+  }
+}
+  
